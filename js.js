888617b3b63638418f32e572c940ecddbf61a79d
anja5772 <103260113+anja5772@users.noreply.github.com>
2022-12-09 08:32:03 +0100

    neue seite

--- js.js
+++ js.js
@@ -14,6 +14,7 @@ document.querySelector('#email').addEventListener('focus', function(e) {
             easing: 'easeOutQuart'
         }
     });
+
 });
 document.querySelector('#password').addEventListener('focus', function(e) {
     if (current) current.pause();
